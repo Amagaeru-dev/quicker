1ea0e35d538b0268fabc0a8ca7248cbb51414af5
Tree-frog-code <konpebesu@gmail.com>
2020-03-16 00:13:17 +0900

    input_stdin()からtrans()を呼び出せない

--- tests/trans.py
+++ tests/trans.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from googletrans import Translator
+import sys
 
 """翻訳をするための単純なAPIを提供するモジュール
 'googletrans'を使っているから、急に使えなくなるかもしれない。
@@ -20,3 +21,11 @@ def tarns(text, lang="-ja"):
     trans = Translator()
     # 翻訳をする メソッドチェーンでよいかどうかはまた考える。
     return trans.translate(text, dest=lang).text
+
+def input_stdin(lang='ja'):
+    """標準入力を`trans()`に渡す"""
+    trs_lines = None
+    for line in sys.stdin:
+        print(trans(line, lang=lang))
+
+input_stdin()
