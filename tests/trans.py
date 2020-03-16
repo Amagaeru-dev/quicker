5a36e9c577a8b0f3876bba8f381ad9e8a2f30ad6
Tree-frog-code <konpebesu@gmail.com>
2020-03-17 00:34:23 +0900

    翻訳の関数を書き上げた。TODO:を読んでおくこと。

--- tests/trans.py
+++ tests/trans.py
@@ -22,10 +22,26 @@ def tarns(text, lang="-ja"):
     # 翻訳をする メソッドチェーンでよいかどうかはまた考える。
     return trans.translate(text, dest=lang).text
 
-def input_stdin(lang='ja'):
-    """標準入力を`trans()`に渡す"""
-    trs_lines = None
-    for line in sys.stdin:
-        print(trans(line, lang=lang))
 
-input_stdin()
+def input_stdin(lang):
+    """標準入力を`trans()`に渡す
+    TODO:外部の関数を呼び出せるようにする。
+    """
+
+    def inner_trans(text, lang=lang):
+        """input_stdin()内のtrans()
+        引数はtrans()の時から、'text'を除いただけ。
+        """
+        lang = lang.strip("-")
+        trans = Translator()
+        # 翻訳をする メソッドチェーンでよいかどうかはまた考える。
+        return trans.translate(text, dest=lang).text
+
+    trs_lines = ''
+
+    for line in sys.stdin.readlines():
+        trs_lines += inner_trans(line, lang=lang)
+        
+    return trs_lines             
+
+
