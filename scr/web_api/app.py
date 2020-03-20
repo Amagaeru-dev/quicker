65601aff96aca7e26e4f877c51b656d22259b4cf
Tree-frog-code <konpebesu@gmail.com>
2020-03-21 05:21:35 +0900

    webアプリ向けのスクリプトを追加した

---
+++ scr/web_api/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    cont = """Some content"""
+
+    return render_template('index.html', cont=cont)
+
+if __name__ == '__main__':
+    app.run()
